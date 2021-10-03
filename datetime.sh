0d79e91618cbfef1eb354e0959375c9d0e7e472a
vishmay-k <vkarbotkar7@gmail.com>
2021-10-03 13:01:08 +0530

    added datetime

---
+++ datetime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Year=`date +%Y`
+Month=`date +%m`
+Day=`date +%d`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date`
+echo "Year: $Year"
+echo "Month: $Month"
+echo "Day: $Day"
+echo "Hour: $Hour"
+echo "Minute: $Minute"
+echo "Second: $Second"
